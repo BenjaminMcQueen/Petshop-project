4b22a23dacf7948c29a533ea9c0610fff75f3061
BenaSPACE <BenaSPACE@gmail.com>
2023-07-31 21:05:50 -0400

    Forgot to push last time I made changes apparently

--- js/interactions.js
+++ js/interactions.js
@@ -2,7 +2,9 @@ function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar')
     const content = document.querySelector('.main')
     const banner = document.querySelector('.banner')
+    const footer = document.querySelector('.footer')
     sidebar.classList.toggle('open')
     content.classList.toggle('open-main')
     banner.classList.toggle('open-main')
+    footer.classList.toggle('open-main')
 }
