7427209ec8040d2b7f8e7c7ef6b6dc9fd1ae7961
BenaSPACE <BenaSPACE@gmail.com>
2023-08-03 16:56:48 -0400

    Lots of progress, new known bug

---
+++ js/submitButton.js
@@ -0,0 +1,9 @@
+function submitContact(e){
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var message = document.getElementById('message').value
+
+    console.log('Recieved Name: ', name)
+    console.log('Recieved Email: ', email)
+    console.log('Recieved Message: ', message)
+}
