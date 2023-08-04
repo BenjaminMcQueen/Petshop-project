da00b8756c8ca9b3c99996df1c20c816a77ea12a
BenaSPACE <BenaSPACE@gmail.com>
2023-08-04 14:28:41 -0400

    8/4/23

--- js/interactions.js
+++ js/interactions.js
@@ -1,10 +1,4 @@
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar')
-    const content = document.querySelector('.main')
-    const banner = document.querySelector('.banner')
-    const footer = document.querySelector('.footer')
     sidebar.classList.toggle('open')
-    content.classList.toggle('open-main')
-    banner.classList.toggle('open-main')
-    footer.classList.toggle('open-main')
 }
